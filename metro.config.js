573fd804688366456b5a3257483c84de6f3d0d85
Peter Macharia <peterlevis3327@gmail.com>
2026-04-28 13:22:57 +0300

    Switch to Expo Metro config for cloud build compatibility

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,7 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config')
+const {getDefaultConfig} = require('expo/metro-config')
 
-const config = {
-  resolver: {
-    sourceExts: ['js', 'jsx', 'ts', 'tsx', 'json']
-  }
-}
+const config = getDefaultConfig(__dirname)
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config)
+config.resolver.sourceExts.push('cjs')
+
+module.exports = config
